08dc4e4f151cafbb5531158c5eda9e2e360cb222
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-11-09 14:33:10 -0500

    commit tree 6a9c792fb1bb41d9b4a8ecae459f19281d0351b9

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+
+    def talk
+        puts "Hello World!"
+    end 
+    
+    def walk
+        puts "The Person is walking"
+    end 
+end
+liz = Person.new
+
+liz.talk
+liz.walk 
